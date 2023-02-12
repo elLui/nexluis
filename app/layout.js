c3c26d11a41a0f5ee1c614e89daf4ec91160d199
elluis <elluis.codes@gmail.com>
2023-02-11 19:47:05 -0600

    we are on fire!
    initial commit -
    started with styled components and have it working correctly!
    https://beta.nextjs.org/docs/styling/css-in-js#styled-components

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,26 @@
-import './globals.css'
+"use client";
+import GlobalStyle from "@/app/styles/globals";
+import StyledComponentsRegistry from "@/app/styles/lib/StyledComponentsRegistry";
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/typography.css";
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+
+
+export default function RootLayout( { children } ) {
+    return ( <html lang="en">
+    {/*
+     <head /> will contain the components returned by the nearest parent
+     head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+     */ }
+    <head/>
+    <body>
+    {/* GlobalStyle must remain outside StyledComponentsRegistry scope */ }
+    {/* https://beta.nextjs.org/docs/styling/css-in-js#styled-components */ }
+    <GlobalStyle/>
+    <StyledComponentsRegistry>
+        { children }
+    </StyledComponentsRegistry>
+    </body>
+    </html> );
 }
